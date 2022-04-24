e2903f7420794fc5e658b778d6decb576f550db4
MeteHan96 <matinjn@code.edu.az>
2022-04-25 02:30:30 +0400

    Proje dosyası ekle.

---
+++ Extension task/ExtensionMethods.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Extension_task
+{
+    static class ExtensionMethods
+    {
+        
+        public static bool IsEven(this int even)
+        {
+            if (even % 2 == 0)
+            
+                    return true;
+            return false;
+        }
+
+        public static int[] GetValueIndexes(this int[] arr, int a)
+        {
+            int[] indexes = { };
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] == a)
+                {
+                    Array.Resize(ref indexes, indexes.Length+1);
+                    indexes[indexes.Length - 1] = i;
+                    return indexes;
+                }
+                
+                
+            }
+            return indexes;
+
+
+        }
+      
+        public static bool IsOdd(this int odd)
+        {
+            if (odd % 2 == 1)
+
+                return true;
+            return false;
+        }
+
+        public static bool IsContainsDigit(string str)
+        {
+            bool checkDigit = false;
+            foreach (var item in str)
+            {
+                if (char.IsDigit(item))
+                {
+                    checkDigit = true;
+                    return checkDigit;
+                }
+            }
+            return checkDigit;
+
+        }
+        public static int[] GetValueIndexes(this string str, char b)
+        {
+            int[] newArr = { };
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] == b)
+                {
+                    Array.Resize(ref newArr, newArr.Length + 1);
+                    newArr[newArr.Length - 1] = i;
+                }
+            }
+            return newArr;
+
+        }
+    }
+}
---
+++ Extension task/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Extension_task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int.TryParse(Console.ReadLine(), out int a);
+            int.TryParse(Console.ReadLine(), out int b);
+            bool checkeven = a.IsEven();
+            bool checkodd = b.IsOdd();
+            Console.WriteLine(checkeven);
+            Console.WriteLine(checkodd);
+            
+            
+        }
+    }
+}
